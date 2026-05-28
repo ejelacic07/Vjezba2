c2349dfe38c5cec670c5fe2cb4485628a5bb8595
Emilia <ejelacic0@keemail.me>
2026-05-28 19:28:54 +0200

    Dva zadatka za vježbu rada s iznimkama. Pizza Party i ulazak u klub.

---
+++ src/Izbacivac/Main.java
@@ -0,0 +1,33 @@
+package Izbacivac;
+
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        int dob;
+       System.out.println("Dobrodošli u Club Java. ");
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Unesite svoje godine: ");
+        dob = input.nextInt();
+
+        try {
+          provjeriDob(dob);
+        } catch (IllegalArgumentException e) {
+             System.out.println(e.getMessage());
+        }
+
+
+
+    }
+
+    public static String provjeriDob(int dob) throws IllegalArgumentException {
+         if (dob < 18) {
+            throw new IllegalArgumentException("Ulaz zabranjen. Premlad za ulazak.");
+         }
+         System.out.println("Dobrodošli u klub!");
+         return "Dobrodošli u klub!";
+    }
+}
